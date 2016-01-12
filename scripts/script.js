61c9744326ed4ee2adb0523b536487c08417dc9a
kevin mattison and Matthew Newell <mattisonii@yahoo.com>
2016-01-11 16:19:03 -0800

    Make multiple changes.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,71 @@
-$(document).ready(function)( {
-  
+$(function() {
+  $(".zero").click(function() {
+    $(".hidden0").toggle();
+    $(".showing0").toggle();
+  });
+
+    $(".one").click(function() {
+      $(".hidden1").toggle();
+      $(".showing1").toggle();
+    });
+    $(".two").click(function() {
+      $(".hidden2").toggle();
+      $(".showing2").toggle();
+    });
+    $(".three").click(function() {
+      $(".hidden3").toggle();
+      $(".showing3").toggle();
+    });
+    $(".four").click(function() {
+      $(".hidden4").toggle();
+      $(".showing4").toggle();
+    });
+    $(".five").click(function() {
+      $(".hidden5").toggle();
+      $(".showing5").toggle();
+    });
+    $(".six").click(function() {
+      $(".hidden6").toggle();
+      $(".showing6").toggle();
+    });
+
+
+/*
+  $(".two").click(function() {
+    $(".hidden2").toggle();
+    $(".showing2").toggle();
+  });
+/*
+
+$(function() {
+  $("#3").click(function() {
+    $(".showing3").toggle();
+    $(".hidden3").toggle();
+  });
+
+$(function() {
+  $("#4").click(function() {
+    $(".showing4").toggle();
+    $(".hidden4").toggle();
+  });
+
+$(function() {
+  $("#5").click(function() {
+    $(".showing5").toggle();
+    $(".hidden5").toggle();
+  });
+
+$(function() {
+  $("#6").click(function() {
+    $(".showing6").toggle();
+    $(".hidden6").toggle();
+  });
+
+$(function() {
+  $("#7").click(function() {
+    $(".showing7").toggle();
+    $(".hidden7").toggle();
+  });
+*/
+
 });
